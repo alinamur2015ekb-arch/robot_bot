8851bf19ad53b26048aa7ae97bbb3382b7faaa50
alinamur2015ekb-arch <alinamur2015ekb@gmail.com>
2026-06-28 16:31:40 +0500

    Update urls.py

--- django_bot/urls.py
+++ django_bot/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include 
 
 urlpatterns = [
-    path('admin/', admin.site.get_user_id), 
+    path('admin/', admin.site.urls), 
     path('api/', include('robot_app.urls')),
 ]
