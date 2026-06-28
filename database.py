6123b2952a2714cc0a7c0c5c972fc3f2dbbf1169
alinamur2015ekb-arch <alinamur2015ekb@gmail.com>
2026-06-28 15:58:13 +0500

    Update database.py

--- database.py
+++ database.py
@@ -3,7 +3,6 @@ from typing import Optional
 
 
 DB_NAME = "database.db"
-DB_STATUS = "status.db"
 
 async def init_telem():
     async with aiosqlite.connect(DB_NAME) as db:
@@ -12,35 +11,17 @@ async def init_telem():
             id INTEGER PRIMARY KEY,
             temperatura REAL,
             humidity REAL,
+            status REAL,
             time_cleaning INTEGER,
             timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
                          )
                          ''')
         
 
-async def create_telem(temperatura, humidity):
+async def create_telem(temperatura, humidity, status):
     async with aiosqlite.connect(DB_NAME) as db:
         await db.execute("""
             INSERT INTO telem (temperatura, humidity, status) 
-            VALUES (?, ?)
+            VALUES (?, ?, ?)
         """, (temperatura, humidity, status))
-        await db.commit()
-
-
-async def init_status():
-    async with aiosqlite.connect(DB_STATUS) as db:
-        await db.execute('''
-            CREATE TABLE IF NOT EXISTS answer(
-            id INTEGER PRIMARY KEY,
-            statuses REAL
-            )
-                         ''')
-        
-
-async def create_status(statuses):
-    async with aiosqlite.connect(DB_STATUS) as db:
-        await db.execute("""
-            INSERT INTO statuses (statuses) 
-            VALUES (?)
-        """, (statuses))
         await db.commit()
